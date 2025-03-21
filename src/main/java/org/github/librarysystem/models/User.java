bbc882badf0255e4fb9724c4e9e1666a08b50492
Shokhrukh Ernazarov <shokhrukhernazarov@mail.ru>
2025-03-21 13:50:46 +0100

    added initial User abstract class

--- src/main/java/org/github/librarysystem/models/User.java
+++ src/main/java/org/github/librarysystem/models/User.java
@@ -1,4 +1,34 @@
 package org.github.librarysystem.models;
 
-public class User {
+public abstract class User {
+    private int userId;
+    private String name;
+    private String email;
+    private static int id = 1;
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.userId = id;
+        id++;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public abstract void borrowBook(Book book);
+    public abstract void returnBook(Book book);
 }
